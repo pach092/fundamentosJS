d9a977518fd1cec13a3691c26439ddb05424decf
pach092 <pachobernal5@gmail.com>
2018-01-02 12:47:56 -0500

    scope example added (it has an intentional error)

---
+++ scope/scope.js
@@ -0,0 +1,9 @@
+function saludarAPacho(){
+	const nombre = 'Pacho';
+	for (let i = 0; i < 10; i++){
+		console.log(`Hola ${nombre}`);
+	}
+	console.log(`El valor de i es ${i}`);
+}
+
+saludarAPacho();
