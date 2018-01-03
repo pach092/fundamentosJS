76f778f318f33f17cdc69f4798664c8e85e529d5
pach092 <pachobernal5@gmail.com>
2018-01-03 16:13:48 -0500

    ECMAScript using Babel example added

---
+++ ECMA/ecma.js
@@ -0,0 +1,10 @@
+// code in ES2016
+[1, 2, 3].forEach((number) => console.log(number));
+
+
+// compatible code using BABEL
+/*"use strict";
+
+[1, 2, 3].forEach(function (number) {
+  return console.log(number);
+});*/
