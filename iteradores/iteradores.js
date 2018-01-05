c81772c672fb2b064e90a5c89e538d5ae4928130
pach092 <pachobernal5@gmail.com>
2018-01-04 23:47:17 -0500

    reset added to ierator.js

--- iteradores/iteradores.js
+++ iteradores/iteradores.js
@@ -1,7 +1,8 @@
 function fibonacci() {
 	let a = 0, b = 1;
 	return {
-		next: function () {
+		next: function (reset) {
+			if (reset) a = 0, b = 1;
 			let f = a;
 			a = b;
 			b += f;
@@ -11,9 +12,4 @@ function fibonacci() {
 }
 
 const fibo = fibonacci();
-fibo.next().value;
-fibo.next().value;
-fibo.next().value;
-fibo.next().value;
-fibo.next().value;
 fibo.next().value;
