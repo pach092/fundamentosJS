13d89eff4d1d2c23dcc846daa378cdde57500b25
pach092 <pachobernal5@gmail.com>
2018-01-04 19:07:14 -0500

    Memorization example added

---
+++ memorization/memorization.js
@@ -0,0 +1,6 @@
+function fibonacci(num, memoria = {}) {
+	if (memoria[num]) return memoria[num];
+	if (num == 1) return 0;
+	if (num == 2) return 1;
+	return memoria[num] = fibonacci(num - 1, memoria) + fibonacci(num - 2, memoria);
+}
